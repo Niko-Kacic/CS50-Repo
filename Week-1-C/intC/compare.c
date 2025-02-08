d4a543efc6757f5c488503a4b5b1f2565b2d6f83
NikoBoolean <niko.kacic@gmail.com>
2025-02-07 23:14:48 -0300

    comparison of two numbers with if in C using scanf, something small but interesting

--- Week-1-C/intC/compare.c
+++ Week-1-C/intC/compare.c
@@ -4,16 +4,30 @@
 int main(void) {
     
     setlocale(LC_ALL, "");
-
-
-    int x;
+    
+    int numberX;
+    int numberY;
+    
     printf("Ingresa un número: ");
-    if (scanf("%d", &x)!=1){
+    if (scanf("%d", &numberX)!=1){
         printf("Entrada invalidad, debe ser un número");
         return 1;
-    } else{
-        printf("El valor es %d", x);
     }
+    printf("Ingresa el segundo número: ");
+    if (scanf("%d", &numberY)!=1){
+        printf("Entrada invalidad, debe ser un número mi pana");
+        return 1;
+    }
+
+    if(numberX > numberY){
+        printf("El valor %d es mayor que %d ", numberX, numberY);
+    }else if(numberX < numberY){
+        printf("El valor %d es menor que %d ", numberX, numberY);
+    }else {
+        printf("Los valores %d y %d son iguales ", numberX, numberY);
+    }
+
+    
     
     return 0;
 
