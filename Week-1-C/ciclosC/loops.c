93ba37ea41ee18dced400bd7c1d426aed7e1b2ba
NikoBoolean <niko.kacic@gmail.com>
2025-02-08 20:50:04 -0300

    applying function usage and the use of so prototype with the for loop

--- Week-1-C/ciclosC/loops.c
+++ Week-1-C/ciclosC/loops.c
@@ -1,20 +1,27 @@
 #include <stdio.h>
 #include <locale.h>
 
+void counter(int n);//Prototipo de la función
 
 int main(void){
     setlocale(LC_ALL, "");
 
-    int counter = 3;
+    // int counter = 3;
 
-    while(counter > 0){
-        printf("Menos %d \n",counter);
-        counter--;
-    }
+    // while(counter > 0){
+    //     printf("Menos %d \n",counter);
+    //     counter--;
+    // }
 
-    for ( int i = 0; i <= 5; i++){
-        printf("Contando y sumando %d \n",i);
-    }
+    counter(10);
 
     return 0;
+}
+
+void counter(int n){
+
+    for ( int i = 0; i <= n; i++){
+        printf("counting in total: %d\n", i);
+    }
+
 }
