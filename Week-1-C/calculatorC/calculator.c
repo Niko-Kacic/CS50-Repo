de6926c63c658c036719064c7e28505b39019dc8
NikoBoolean <niko.kacic@gmail.com>
2025-02-10 16:33:09 -0300

    las function added

--- Week-1-C/calculatorC/calculator.c
+++ Week-1-C/calculatorC/calculator.c
@@ -15,7 +15,8 @@ int validarEntradaDouble(double *input);
 
 void addNums(Numbers *numbers);
 void sustracNums(Numbers *numbers);
-void mutiplyNum(Numbers *numbers);
+void multiplyNum(Numbers *numbers);
+void divideNum(Numbers *numbers);
 
 int main(void){
     setlocale(LC_ALL, "");
@@ -23,28 +24,39 @@ int main(void){
     Numbers numbers;
 
     do {
-        printf("Ingrese una opción 1 o 2: \n");
+        printf("Ingrese una opción: \n1-Sumar \n2-Restar \n3-Multiplicar \n4-Dividir \n5-Salir \n->: ");
         if (!validarEntradaInt(&numbers.choice)) {
             continue;
         }else {
             switch (numbers.choice) {
                 case 1:
-                    printf("Opción uno\n");
+                    printf("Opción uno Sumar\n");
                     addNums(&numbers);
                     printf("La suma de %.1lf y %.1lf es: %.1lf\n", numbers.num1, numbers.num2, numbers.result);
                     numbers.choice = 0;
                     break;
                 case 2:
-                    printf("Opción dos\n");
+                    printf("Opción dos Restar\n");
                     sustracNums(&numbers);
                     printf("La resta de %.1lf y %.1lf es: %.1lf\n", numbers.num1, numbers.num2, numbers.result);
                     numbers.choice = 0;
                     break;
                 case 3:
-                    printf("Opción tres\n");
-                    mutiplyNum(&numbers);
+                    printf("Opción tres Multiplicar\n");
+                    multiplyNum(&numbers);
                     printf("La multiplicación de %.1lf y %.1lf es: %.1lf\n", numbers.num1, numbers.num2, numbers.result);
                     numbers.choice = 0;
+                    break;
+                case 4:
+                    printf("Opción cuatro Dividir\n");
+                    divideNum(&numbers);
+                    printf("La divición de %.1lf y %.1lf es: %.1lf\n", numbers.num1, numbers.num2, numbers.result);
+                    numbers.choice = 0;
+                    break;
+                case 5:
+                    printf("Adios! ");
+                    numbers.choice =0;
+                    break;
                 default:
                     printf("Opción no válida\n");
                     break;
@@ -71,6 +83,7 @@ int validarEntradaInt(int *input) {
 }
 
 int validarEntradaDouble(double *input) {
+    printf("Ingrese un número: ");
     if (scanf("%lf", input) != 1) {
         printf("Ingrese un número válido\n");
         cleanBuffer();
@@ -94,9 +107,16 @@ void sustracNums(Numbers *numbers) {
     numbers->result = numbers->num1 - numbers->num2;
 }
 
-void mutiplyNum(Numbers *numbers){
+void multiplyNum(Numbers *numbers){
     while (!validarEntradaDouble(&numbers->num1));
     while (!validarEntradaDouble(&numbers->num2));
 
     numbers->result = numbers->num1 * numbers->num2;
+}
+
+void divideNum(Numbers *numbers){
+    while (!validarEntradaDouble(&numbers->num1));
+    while (!validarEntradaDouble(&numbers->num2));
+
+    numbers->result = numbers->num1 / numbers->num2;
 }
