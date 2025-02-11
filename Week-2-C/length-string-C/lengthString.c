7b43be25da3c7f7e7ee5602a696173dd80c3be6b
NikoBoolean <niko.kacic@gmail.com>
2025-02-11 18:48:13 -0300

    improved string measurement without the need of a loop

--- Week-2-C/length-string-C/lengthString.c
+++ Week-2-C/length-string-C/lengthString.c
@@ -4,7 +4,7 @@
 int main(void){
 
     char lengthString[50];
-    int n =0 ;
+
 
     printf("Ingresa una palabra: ");
     fgets(lengthString, 50, stdin);
@@ -12,13 +12,12 @@ int main(void){
     size_t lenString = strlen(lengthString);
     if (lenString > 0 && lengthString[lenString-1]== '\n'){
         lengthString[lenString-1] = '\0';
+        lenString--;
     }
 
-    while(lengthString[n]!=0){
-        n++;
-    }
 
-    printf("El largo la palabra %s es de %d caracteres", lengthString, n);
+
+    printf("El largo la palabra %s es de %zu caracteres", lengthString, lenString);
 
     return 0;
 }
