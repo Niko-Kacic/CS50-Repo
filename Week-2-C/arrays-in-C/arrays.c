8b82c8f99f49296384580262ca94e82d1d4e16ff
NikoBoolean <niko.kacic@gmail.com>
2025-02-10 22:02:25 -0300

    function created to calculate average

--- Week-2-C/arrays-in-C/arrays.c
+++ Week-2-C/arrays-in-C/arrays.c
@@ -1,28 +1,40 @@
 #include <stdio.h>
 
 void cleanBuffer();
+float average(int length, int array[]);
+
+const int N = 3;
 
 int main(void) {
-    int scores[3];
+    int scores[N];
 
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < N; i++) {
         while (1) {
-            printf("Score %d :  ", i + 1);
+            printf("Score %d: ", i + 1);
             if (scanf("%d", &scores[i]) != 1) {
                 printf("Ingrese un número válido\n");
                 cleanBuffer();
             } else {
-                break;
+                break;  
             }
         }
     }
 
-    printf("Average: %.1lf\n", (scores[0] + scores[1] + scores[2]) / 3.0);
+    printf("Average: %.1lf\n", average(N, scores));
     return 0;
 }
 
 void cleanBuffer() {
     int c;
     while ((c = getchar()) != '\n' && c != EOF) {
+        // Limpiar el buffer de entrada
     }
+}
+
+float average(int length, int array[]) {
+    int total = 0;
+    for (int i = 0; i < length; i++) {
+        total += array[i];
+    }
+    return total / (float) length;
 }
